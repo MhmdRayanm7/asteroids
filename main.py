af5c5338dcdcd08e6a46c2c73ebaac7a32f854ea
MhmdRayanM7 <m7mddotm7@gmail.com>
2025-11-25 11:03:30 +0200

    add 'FPS' to main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -17,6 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) /1000
 
 
 if __name__ == "__main__":
